3deda060b7496891477b3e105dc936e4beaee36d
mev <mev@PC-123-18>
2023-03-13 14:24:18 +0100

    Dodali smo metodu vrati vrijeme

--- Controllers/PrviController.cs
+++ Controllers/PrviController.cs
@@ -36,5 +36,9 @@ namespace Paup_2023.Controllers
             return View();
 
         }
+        public string VratiVrijeme()
+        {
+            return DateTime.Now.ToString();
+        }
     }
 }
